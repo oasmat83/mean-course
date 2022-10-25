cd907f8402e2bc3db71f984685efee575a0f57c3
Oscar Asmatm <oasmat@worldox.com>
2022-10-25 10:10:08 -0400

    update

---
+++ src/app/posts/post-create/post-create-component.ts
@@ -0,0 +1,23 @@
+import { NgFor } from "@angular/common";
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { PostsService } from '../post.service';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create-component.html',
+  styleUrls: ['./post-create-component.scss']
+})
+
+export class PostCreateComponent {
+  enteredContent = "";
+  enteredTitle = "";
+
+  constructor(public postsService: PostsService) {}
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) return;
+    this.postsService.addPost(form.value.title, form.value.content)
+  }
+
+}
